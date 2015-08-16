4b61a608e3d6be20e746b20fd6ae19bbb56e1b75
rfaldetta <ross.faldetta@gmail.com>
2015-08-15 21:58:47 -0500

    completes part 2

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,7 @@
 class Venue < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => { :scope => :address }
   validates :neighborhood_id, :presence => true
+
+  belongs_to :neighborhood
+  has_many :favorites
 end
