1c089866553178950624e726a58d0ce8d7f5f746
rfaldetta <ross.faldetta@gmail.com>
2015-08-15 22:16:04 -0500

    completes part 3 dropdowns

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,5 @@
 class Neighborhood < ActiveRecord::Base
-  validates :name, :presence => true, :uniqueness { :scope => :city}
+  validates :name, :presence => true, :uniqueness => { :scope => :city }
 
   has_many :venues
 end
